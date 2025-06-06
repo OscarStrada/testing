4a1d5b4f9a6b68228c131f12f8ced6b929ad86e3
Oscar Estrada <oestrada@ia.com.mx>
2025-06-06 13:29:42 -0600

    feat(test): create custom render

--- src/pages/__tests__/ProductsPage.spec.tsx
+++ src/pages/__tests__/ProductsPage.spec.tsx
@@ -1,9 +1,15 @@
-import { render, screen } from "@testing-library/react";
+import { ReactNode } from "react";
+import { render, RenderResult, screen } from "@testing-library/react";
 import { test } from "vitest";
-import App from "../../App";
+import { AppProvider } from "../../context/AppProvider";
+import { ProductsPage } from "../ProductsPage";
 
 test("Loads and displays title", async () => {
-    render(<App />);
+    renderComponent(<ProductsPage />);
 
     await screen.findAllByRole("heading", { name: "Product price updater" });
 });
+
+function renderComponent(component: ReactNode): RenderResult {
+    return render(<AppProvider>{component}</AppProvider>);
+}
